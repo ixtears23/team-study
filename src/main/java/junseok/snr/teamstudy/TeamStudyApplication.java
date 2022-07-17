bdfa805648533d476299497e56c9491699aacd1e
junseokoh0 <junseok@bodycodi.com>
2022-07-18 00:01:21 +0900

    Git Initial Setting

---
+++ src/main/java/junseok/snr/teamstudy/TeamStudyApplication.java
@@ -0,0 +1,13 @@
+package junseok.snr.teamstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junseok/snr/teamstudy/TeamStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.teamstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
